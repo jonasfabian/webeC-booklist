4840e38e6a6087b10c3de2d40a205cd8cfeb6dec
Jonas Fabian <jonas.fabian@students.fhnw.ch>
2021-10-18 10:24:23 +0200

    excercise done

--- src/main/java/ch/fhnw/webec/booklist/controller/AboutController.java
+++ src/main/java/ch/fhnw/webec/booklist/controller/AboutController.java
@@ -1,6 +1,7 @@
 package ch.fhnw.webec.booklist.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,11 +9,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class AboutController {
     @ResponseBody
-    @RequestMapping(value = "/about", method = RequestMethod.GET)
+    @GetMapping("/about")
+    // Does basically the same as @GetMapping("/about")
+    // @RequestMapping(value = "/about", method = RequestMethod.GET)
     public String about() {
         // language=HTML
         return """
-            <div>Hello Guys!</div>
-            """;
+                <div>Hello Guys!</div>
+                """;
     }
 }
